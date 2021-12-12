c1ecf0f31f56ab27ed8ce89085dc29a8016c1fda
woeiyih <74339370+woeiyih@users.noreply.github.com>
2021-12-12 23:39:19 +0800

    SQL Script for Database Set-Up

---
+++ woeiyih_ETI_assg1_mysql.sql
@@ -0,0 +1,11 @@
+/*CREATE database my_db;
+
+USE my_db;
+
+CREATE TABLE Passengers (ID varchar (5) NOT NULL PRIMARY KEY, FirstName VARCHAR(30), LastName VARCHAR(30), MobileNo VARCHAR(8), Email VARCHAR(30)); 
+
+INSERT INTO Passengers (ID, FirstName, LastName, MobileNo, Email) VALUES ("0001", "Logan", "Peele", "81112222", "lp@gmail.com");
+INSERT INTO Passengers (ID, FirstName, LastName, MobileNo, Email) VALUES ("0002", "Jake", "Peele", "91112222", "jp@gmail.com");
+INSERT INTO Passengers (ID, FirstName, LastName, MobileNo, Email) VALUES ("0003", "Bo Song", "Tim", "82223333", "tbs@gmail.com");
+*/
+select * FROM Passengers
